249cddbf4bb9e99b421d0b4578d2d32cda02caf5
christopher-dly <christopher.dolley1@gmail.com>
2025-04-23 10:55:30 +0200

    maj

--- migrations/Version20250421205623.php
+++ migrations/Version20250421205623.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250407114720 extends AbstractMigration
+final class Version20250421205623 extends AbstractMigration
 {
     public function getDescription(): string
     {
