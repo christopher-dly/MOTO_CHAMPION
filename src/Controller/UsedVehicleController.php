73df0e92803c5912c39470b736076298bf9330f8
christopher-dly <christopher.dolley1@gmail.com>
2025-05-11 23:04:32 +0200

    maj

--- src/Controller/UsedVehicleController.php
+++ src/Controller/UsedVehicleController.php
@@ -2,14 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\UsedVehicleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UsedVehicleController extends AbstractController
 {
     #[Route('/used-vehicle', name: 'UsedVehicle')]
-    public function usedVehicle()
+    public function usedVehicle(UsedVehicleRepository $repo): Response
     {
-        return $this->render('pages/used_Vehicle.html.twig');
+        $usedVehicles = $repo->findAll();
+
+        return $this->render('pages/used_vehicle.html.twig', [
+            'usedVehicles' => $usedVehicles,
+        ]);
     }
 }
